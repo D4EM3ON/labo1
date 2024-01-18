4c5236d82959c9b51e9b8f02511c0a72e4255dd3
D4EM3ON <76664983+D4EM3ON@users.noreply.github.com>
2024-01-18 09:35:28 -0500

    printf bug

--- labo1.s
+++ labo1.s
@@ -11,7 +11,7 @@ main:
     adr   x1, nombre                //
     bl    scanf                     // scanf(&fmtEntree, &nombre)
     ldr   x19, nombre               // a = nombre
-    bl printf
+    //bl printf
     // Lire b
     //bl scanf
     //ldr x20, nombre // b = nombre
